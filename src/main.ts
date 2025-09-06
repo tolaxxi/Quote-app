47be5aaa8577c073bbaebb5fb284234e7988e767
Tolani Onajoko <omotolanionajoko@gmail.com>
2025-09-06 20:39:53 +0100

    feat: add tweet quote functionality

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ const quote = document.querySelector<HTMLParagraphElement>('#QuoteDisplay')!;
 const newQuoteBtn = document.querySelector<HTMLButtonElement>('#newQuoteBtn')!;
 const speakBtn = document.querySelector<HTMLButtonElement>('#ReadAloud')!;
 const copyBtn = document.querySelector<HTMLButtonElement>('#copyBtn')!;
+const tweetBtn = document.querySelector<HTMLButtonElement>('#tweetBtn')!;
 
 let Quote = '';
+let Author = '';
 
 getQuote();
 
@@ -20,8 +22,10 @@ async function getQuote(): Promise<void> {
     const data = await res.json();
     if (Array.isArray(data)) {
       Quote = data[0].content;
+      Author = data[0].author;
+
       quote.textContent = Quote;
-      author.textContent = data[0].author;
+      author.textContent = Author;
     } else {
       quote.textContent = data.content;
       author.textContent = data.author;
@@ -35,7 +39,7 @@ async function getQuote(): Promise<void> {
 speakBtn.addEventListener('click', () => {
   const speakQuote = Quote;
   const utterance = new SpeechSynthesisUtterance(speakQuote);
-
+  window.speechSynthesis.cancel();
   const synth = window.speechSynthesis;
   synth.speak(utterance);
 });
@@ -46,3 +50,11 @@ copyBtn.addEventListener('click', () => {
   navigator.clipboard.writeText(Quote);
 });
 
+// post quote as tweet
+tweetBtn.addEventListener('click', () => {
+  const tweetText = `${Quote} - ${Author}`;
+
+  const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
+
+  window.open(tweetUrl, '_blank');
+});
