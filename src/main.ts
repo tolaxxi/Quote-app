c104e861bb3c14b8226896079171a36933729e65
Tolani Onajoko <omotolanionajoko@gmail.com>
2025-09-06 20:11:49 +0100

    feat: add read quote functionality

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ const API_URL = 'https://api.realinspire.live/v1/quotes/random?maxLength=100';
 const author = document.querySelector<HTMLParagraphElement>('#QuoteAuthor')!;
 const quote = document.querySelector<HTMLParagraphElement>('#QuoteDisplay')!;
 const newQuoteBtn = document.querySelector<HTMLButtonElement>('#newQuoteBtn')!;
+const speakBtn = document.querySelector<HTMLButtonElement>('#ReadAloud')!;
+
+let Quote = '';
 
 getQuote();
+
 // fetch quote from api
 newQuoteBtn.addEventListener('click', () => {
   getQuote();
@@ -14,7 +18,8 @@ async function getQuote(): Promise<void> {
     const res = await fetch(API_URL);
     const data = await res.json();
     if (Array.isArray(data)) {
-      quote.textContent = data[0].content;
+      Quote = data[0].content;
+      quote.textContent = Quote;
       author.textContent = data[0].author;
     } else {
       quote.textContent = data.content;
@@ -24,3 +29,12 @@ async function getQuote(): Promise<void> {
     quote.textContent = 'please Check your Connection and Try Again';
   }
 }
+
+// read quote aloud
+speakBtn.addEventListener('click', () => {
+  const speakQuote = Quote;
+  const utterance = new SpeechSynthesisUtterance(speakQuote);
+
+  const synth = window.speechSynthesis;
+  synth.speak(utterance);
+});
